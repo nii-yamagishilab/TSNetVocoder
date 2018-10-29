6050521b0c9f6ad0b8a87022597d9337e647f719
Shinji Takaki <takaki@nii.ac.jp>
2018-10-29 14:49:06 +0900

    add samples

--- module/__init__.py
+++
@@ -1,8 +0,0 @@
-# ==============================================================================
-# Copyright (c) 2018, Yamagishi Laboratory, National Institute of Informatics
-# Author: Shinji Takaki (takaki@nii.ac.jp)
-# All rights reserved.
-# ==============================================================================
-
-from .model import Model
-from .speechprocessing import SpeechProcessing
